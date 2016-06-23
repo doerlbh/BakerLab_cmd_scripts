d084a1527a7f58c064337e2406c6c45d07cd7d75
doerlbh <doerlbh@gmail.com>
2016-06-23 11:28:22 -0700

    update python project

---
+++  pdb_cccp_extraction.py
@@ -0,0 +1,31 @@
+# pre: a list of pdb file names with parameters
+# post: extract out parameters of protein design and reassign into new combo
+# by Baihan Lin, June 2016
+
+# read in the list and parse the parameters
+
+
+for bb in bbset:
+    A = bb[:]
+    B = bb[:]
+    C = bb[:]
+    Z = bb[:]
+    
+    tel = {'jack': 4098, 'sape': 4139}
+           tel['guido'] = 4127
+>>> tel
+
+
+# sample finely around the parameters
+
+
+
+# remove duplicates
+
+
+
+
+# make combinations
+
+
+
