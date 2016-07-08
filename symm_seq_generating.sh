68592240b0ffb709bf98551ef6d2b391750012fe
doerlbh <doerlbh@gmail.com>
2016-07-08 10:41:50 -0700

    update code

--- symm_seq_generating.sh
+++ symm_seq_generating.sh
@@ -12,12 +12,12 @@ grep "/gscratch/stf/sunnylin/160624_flatland_finer_sampling/3/" 20160706_uniqdon
 grep "/gscratch/stf/sunnylin/160624_flatland_finer_sampling/2/" 20160706_uniqdonelist >> 20160706_uniqdonelist_C3
 grep "/gscratch/stf/sunnylin/160624_flatland_finer_sampling/1/" 20160706_uniqdonelist >> 20160706_uniqdonelist_C3
 
-cp 20160706_uniqdonelist_C2 C2_sym.s
-cp 20160706_uniqdonelist_C3 C3_sym.s
+cp 20160706_uniqdonelist_C2 C2_sym.sh
+cp 20160706_uniqdonelist_C3 C3_sym.sh
 
 sed -i -e 's/^/cd /' *_sym.sh
-sed -i 's#$# ;for i in HBNet*pdb;do /gscratch/dimaio/zibochen/Rosetta/main/source/bin/symm_seq_gen_2D.default.linuxgccrelease -s $i *adjacent.pdb -cn 3;done>symm_seq.log#' C3_sym.sh
-sed -i 's#$# ;for i in HBNet*pdb;do /gscratch/dimaio/zibochen/Rosetta/main/source/bin/symm_seq_gen_2D.default.linuxgccrelease -s $i *adjacent.pdb -cn 2;done>symm_seq.log#' C2_sym.sh
+sed -i 's#$# ;for i in HBNet*pdb;do /gscratch/stf/zibochen/Rosetta/main/source/bin/symm_seq_gen_2D.default.linuxgccrelease -s $i *adjacent.pdb -cn 3;done>symm_seq.log#' C3_sym.sh
+sed -i 's#$# ;for i in HBNet*pdb;do /gscratch/stf/zibochen/Rosetta/main/source/bin/symm_seq_gen_2D.default.linuxgccrelease -s $i *adjacent.pdb -cn 2;done>symm_seq.log#' C2_sym.sh
 
 # cd /gscratch/stf/sunnylin/160624_flatland_finer_sampling/5/1/89/290 ;for i in HBNet*pdb;do /gscratch/dimaio/zibochen/Rosetta/main/source/bin/symm_seq_gen_2D.default.linuxgccrelease -s $i *adjacent.pdb -cn 2;done>symm_seq.log
 # cd /gscratch/stf/sunnylin/160624_flatland_finer_sampling/1/1/43/10 ;for i in HBNet*pdb;do /gscratch/dimaio/zibochen/Rosetta/main/source/bin/symm_seq_gen_2D.default.linuxgccrelease -s $i *adjacent.pdb -cn 3;done>symm_seq.log
---
+++ useful_collection.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#Author: Baihan Lin
+#Lab: Baker Lab
+
+cat /gscratch/stf/sunnylin/160624_flatland_finer_sampling/20160706_donelist | uniq -d > /gscratch/stf/sunnylin/160624_flatland_finer_sampling/20160706_uniqdonelist
+
+grep "/gscratch/stf/sunnylin/160624_flatland_finer_sampling/5/" 20160706_uniqdonelist > 20160706_uniqdonelist_C2
+
+cp 20160706_uniqdonelist_C2 C2_sym.s
+
+sed -i -e 's/^/cd /' *_sym.sh
+sed -i 's#$# ;for i in HBNet*pdb;do /gscratch/dimaio/zibochen/Rosetta/main/source/bin/symm_seq_gen_2D.default.linuxgccrelease -s $i *adjacent.pdb -cn 3;done>symm_seq.log#' C3_sym.sh
+
+cat /gscratch/stf/sunnylin/160624_flatland_finer_sampling/20160706_donelist |wc
+
+ wc -l symm_seq.run 
+
+  for i in `cat rm_reshead`; do cd $i; rm done; done & 
