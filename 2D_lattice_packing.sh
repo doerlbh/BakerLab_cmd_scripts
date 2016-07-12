bbfc6b5c8c5b22b957a07af904619a37059b1433
doerlbh <doerlbh@gmail.com>
2016-07-11 17:29:59 -0700

    update code

--- 2D_lattice_packing.sh
+++ 2D_lattice_packing.sh
@@ -39,21 +39,21 @@ sed -i 's#$# ;for i in HBNet*res; do echo "">>$i;cat /gscratch/stf/zibochen/temp
 cat res_C3_1_12.sh res_C3_2_13.sh res_C3_3_6.sh res_C3_4_1.sh res_C2_1_31.sh res_C2_2_16.sh > make_res.sh
 
 cp sildir20160711.list packing1.run;
-rsub packing1.run;
+#rsub packing1.run;
 
 sed -i -e 's/^/cd /' packing1.run;
-sed -i 's#$# ;/gscratch/baker/sboyken/AzoF_Rosetta/Rosetta/main/source/bin/rosetta_scripts.hdf5.linuxgccrelease -database /gscratch/baker/sboyken/AzoF_Rosetta/Rosetta/main/database/ @/gscratch/baker/zibochen/scripts/xml_and_flags/flatland_final_packing.flags -in:file:silent #' packing1.run;
-paste packing1.run sil20160711.list > packing2.run;
-sed -i 's#$#  -parser:script_vars resfile=#' packing2.run;
-paste packing2.run res20160711.list > packing3.run
+sed -i 's#$#;/gscratch/baker/sboyken/AzoF_Rosetta/Rosetta/main/source/bin/rosetta_scripts.hdf5.linuxgccrelease -database /gscratch/baker/sboyken/AzoF_Rosetta/Rosetta/main/database/ @/gscratch/baker/zibochen/scripts/xml_and_flags/flatland_final_packing.flags -in:file:silent #' packing1.run;
+cat packing1.run | tr -s " " > packing2.run;
+paste packing2.run sil20160711.list > packing3.run;
+sed -i 's#$#  -parser:script_vars resfile=#' packing3.run;
+paste packing3.run res20160711.list > packing4.run
+sed -i 's#$#;#' packing4.run;
+cat packing4.run | tr -s " " > packing5.run;
 
-sudo pssu --create-set 2Dpacking
-
- cat packing3.run |psu --load --sql-set 2Dpacking
-
- psu --stat --sql-set 2Dpacking
- 
- for i in `seq 60`;do qsub submit_packing -q bf;done >JOB_IDs_2Dpacking
+sudo pssu --create-set 2Dpacking;
+cat packing5.run |psu --load --sql-set 2Dpacking;
+psu --stat --sql-set 2Dpacking;
+for i in `seq 60`;do qsub submit_packing -q bf;done >JOB_IDs_2Dpacking;
 
 # cd /gscratch/stf/sunnylin/160624_flatland_finer_sampling/5/1/89/203 ;/gscratch/baker/sboyken/AzoF_Rosetta/Rosetta/main/source/bin/rosetta_scripts.hdf5.linuxgccrelease -database /gscratch/baker/sboyken/AzoF_Rosetta/Rosetta/main/database/ @/gscratch/baker/zibochen/scripts/xml_and_flags/flatland_final_packing.flags -in:file:silent 	/gscratch/stf/sunnylin/160624_flatland_finer_sampling/5/1/89/203/HBNet_C211_C211_62.2_47.8_740_A_ZC16_adjacent_0001_0001_designed_full_lattice.silent  -parser:script_vars resfile=	/gscratch/stf/sunnylin/160624_flatland_finer_sampling/5/1/89/203/HBNet_C211_C211_62.2_47.8_740_A_ZC16_adjacent_0001_0001_designed_full_lattice.res
 
