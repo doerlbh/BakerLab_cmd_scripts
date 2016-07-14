15ba0683850001d8c15ff6c184cced9337e76ddf
doerlbh <doerlbh@gmail.com>
2016-07-13 17:18:34 -0700

    update code

--- asym_packing_prep.sh
+++ asym_packing_prep.sh
@@ -5,9 +5,14 @@
 
 cd /gscratch/stf/sunnylin/160624_flatland_finer_sampling/;
 
-find `pwd` -name "packed*.pdb" > packed20160712.list 
+find `pwd` -name "extracted_adjacent.pdb" > clrjunk.sh;
+sed -i -e 's/^/rm /' clrjunk.sh;
+sh clrjunk.sh;
 
+
+find `pwd` -name "packed*.pdb" > packed20160712.list 
 for i in `cat packed20160712.list`;do dirname $i;done > packeddir20160712.list
+cat packdir20160712.list | uniq -d > packdir20160712uniq.list
 
 #wc -l res20160711.list 
 #wc -l sil20160711.list 
@@ -18,14 +23,14 @@ for i in `cat packed20160712.list`;do dirname $i;done > packeddir20160712.list
 
 #cat /gscratch/stf/sunnylin/160624_flatland_finer_sampling/packed20160712.list | uniq -d > /gscratch/stf/sunnylin/160624_flatland_finer_sampling/packed20160712.list
 
-cp packeddir20160712 analydone.run;
+cp packdir20160712uniq.list analydone.run;
 
-grep "/gscratch/stf/sunnylin/160624_flatland_finer_sampling/6/" 20160706_uniqdonelist > extd_6_ZC31_76.sh;
-grep "/gscratch/stf/sunnylin/160624_flatland_finer_sampling/5/" 20160706_uniqdonelist > extd_5_ZC16_75.sh;
-grep "/gscratch/stf/sunnylin/160624_flatland_finer_sampling/4/" 20160706_uniqdonelist > extd_4_5L6HC3_1_85.sh;
-grep "/gscratch/stf/sunnylin/160624_flatland_finer_sampling/3/" 20160706_uniqdonelist > extd_3_2L6HC3_6_74.sh;
-grep "/gscratch/stf/sunnylin/160624_flatland_finer_sampling/2/" 20160706_uniqdonelist > extd_2_2L6HC3_13_74.sh;
-grep "/gscratch/stf/sunnylin/160624_flatland_finer_sampling/1/" 20160706_uniqdonelist > extd_1_2L6HC3_12_74.sh;
+grep "/gscratch/stf/sunnylin/160624_flatland_finer_sampling/6/" packdir20160712uniq.list > extd_6_ZC31_76.sh;
+grep "/gscratch/stf/sunnylin/160624_flatland_finer_sampling/5/" packdir20160712uniq.list > extd_5_ZC16_75.sh;
+grep "/gscratch/stf/sunnylin/160624_flatland_finer_sampling/4/" packdir20160712uniq.list > extd_4_5L6HC3_1_85.sh;
+grep "/gscratch/stf/sunnylin/160624_flatland_finer_sampling/3/" packdir20160712uniq.list > extd_3_2L6HC3_6_74.sh;
+grep "/gscratch/stf/sunnylin/160624_flatland_finer_sampling/2/" packdir20160712uniq.list > extd_2_2L6HC3_13_74.sh;
+grep "/gscratch/stf/sunnylin/160624_flatland_finer_sampling/1/" packdir20160712uniq.list > extd_1_2L6HC3_12_74.sh;
 
 # cd <dir>;for i in HBNet*pdb;do sh /gscratch/stf/sunnylin/160624_flatland_finer_sampling/extract4docking/get_adj.sh $i packed_${i/.pdb/}*designed_full_lattice* <monomer_length>;done
 
@@ -44,7 +49,7 @@ cat extd_6_ZC31_76.sh extd_5_ZC16_75.sh extd_4_5L6HC3_1_85.sh extd_3_2L6HC3_6_74
 # cd /gscratch/stf/sunnylin/160624_flatland_finer_sampling/extract4docking/204;for i in HBNet*pdb;do sh /gscratch/stf/sunnylin/160624_flatland_finer_sampling/extract4docking/get_adj.sh $i packed_${i/.pdb/}*designed_full_lattice* 75;done
 
 #submit jobs to parallel
-cat extractdone.run | parallel -j16 &
+cat extractdone.run | parallel -j16 &;
 
  # cd <dir>;/gscratch/baker/sboyken/AzoF_Rosetta/Rosetta/main/source/bin/rosetta_scripts.hdf5.linuxgccrelease -database /gscratch/baker/sboyken/AzoF_Rosetta/Rosetta/main/database/ -beta -out:prefix analysis_ -parser:protocol /gscratch/stf/sunnylin/160624_flatland_finer_sampling/extract4docking/analysis.xml -s *extracted*pdb -renumber_pdb 1;
 
