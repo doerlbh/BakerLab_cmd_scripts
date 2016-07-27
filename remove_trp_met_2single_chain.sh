1e86cd02a1b82231cc7f1d2afa5c66df8f6796b3
doerlbh <doerlbh@gmail.com>
2016-07-27 10:38:57 -0700

    update code

--- remove_trp_met_2single_chain.sh
+++ remove_trp_met_2single_chain.sh
@@ -6,24 +6,26 @@
 cd /work/sunnylin/self_assembly_design/modeltest/4_remove_met_trp/;
 for i in *pdb;do
 	mkdir ${i%.*};
+	cp $i ${i%.*};
 	cd ${i%.*};
 	cp ../design.res ${i%.*}_design.res;
+	cp ../design.res temp_design.res;
 	cp ../heterodimer_final_design.flags .;
 	cp ../heterodimer_final_design.xml ${i%.*}_heterodimer_final_design.xml;
-	sed -i 's#/work/sunnylin/self_assembly_design/modeltest/4_remove_met_trp/design.res#/work/sunnylin/self_assembly_design/modeltest/4_remove_met_trp/${i%.*}/${i%.*}_design.res#g' ${i%.*}_heterodimer_final_design.xml;
-	grep "MET A " $i > metA_change;
-	grep "MET B " $i > metB_change;
-	cut -c20-22 metA_change;
+	sed -i "s#/work/sunnylin/self_assembly_design/modeltest/4_remove_met_trp/design.res#/work/sunnylin/self_assembly_design/modeltest/4_remove_met_trp/${i%.*}/${i%.*}_design.res#g" ${i%.*}_heterodimer_final_design.xml;
+	grep "MET A " $i | cut -c24-26 | sed 's#$# A NOTAA M#' >> temp_design.res;
+	grep "MET B " $i | cut -c24-26 | sed 's#$# B NOTAA M#' >> temp_design.res;
+	grep "TRP A " $i | cut -c24-26 | sed 's#$# A NOTAA W#' >> temp_design.res;
+	grep "TRP B " $i | cut -c24-26 | sed 's#$# B NOTAA W#' >> temp_design.res;
+	uniq temp_design.res > ${i%.*}_design.res;
+	cd ../;
 done
 
 #240 B NOTAA W
 
 for i in */; do
-	cd $i
+	cd $i;
 
-/work/zibochen/Rosetta/main/source/bin/rosetta_scripts.default.linuxgccrelease -s input.pdb @heterodimer_final_design.flags -parser:protocol /work/sunnylin/self_assembly_design/modeltest/4_remove_met_trp/heterodimer_final_design.xml
-
-
-
-<ReadResfile name=resfile filename="/work/sunnylin/self_assembly_design/modeltest/4_remove_met_trp/design.res" />
-
+/work/zibochen/Rosetta/main/source/bin/rosetta_scripts.default.linuxgccrelease -s input.pdb @heterodimer_final_design.flags -parser:protocol *.xml
+cd ../;
+done
