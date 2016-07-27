52af1442544ec30a20ddb6743f65ea982b9bebe2
doerlbh <doerlbh@gmail.com>
2016-07-27 15:21:58 -0700

    update code

--- preorder_trimer.sh
+++ preorder_trimer.sh
@@ -6,11 +6,11 @@
 cd /work/sunnylin/self_assembly_design/model_preorder/selected/;
 for i in */;do
 cd $i;
-for i in *.pdb; do
-	grep "^ATOM..* B " $i > "B_$i"
-	grep "^ATOM..* A " $i > "A_$i"
+for j in *.pdb; do
+grep "^ATOM..* A " $j > "A_$j"
+grep "^ATOM..* B " $j > "B_$j"
 done
-
-python /work/sunnylin/self_assembly_design/flatland_add_loop_from_pdb_trimer.py
+python /work/sunnylin/self_assembly_design/flatland_add_loop_from_pdb_trimer.py /work/sunnylin/self_assembly_design/benchmark/bobby_closed/XAAA_heteromonomer_ala.pdb A*.pdb A_$i_looped.pdb;
+python /work/sunnylin/self_assembly_design/flatland_add_loop_from_pdb_trimer.py /work/sunnylin/self_assembly_design/benchmark/bobby_closed/XAAA_heteromonomer_ala.pdb B*.pdb A_$i_looped.pdb;
 cd ../;
 done
